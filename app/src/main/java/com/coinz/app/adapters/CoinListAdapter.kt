5bc26c1b3ee059a2afba63a99e5fff908b87e60f
smueksch <sebastian.mueksch@gmail.com>
2018-11-30 21:27:29 +0000

    Designed a proper coin card for LocalWallet with buttons to send and store the coin.

--- app/src/main/java/com/coinz/app/adapters/CoinListAdapter.kt
+++ app/src/main/java/com/coinz/app/adapters/CoinListAdapter.kt
@@ -1,6 +1,7 @@
 package com.coinz.app.adapters
 
 import android.content.Context
+import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -13,11 +14,13 @@ class CoinListAdapter(context: Context): RecyclerView.Adapter<CoinListAdapter.Co
 
     inner class CoinViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
-        val coinItemView: TextView = itemView.findViewById(R.id.textView)
+        val coinCurrency: TextView = itemView.findViewById(R.id.coin_card_currency)
+        val coinId: TextView = itemView.findViewById(R.id.coin_card_id)
+        val coinValue: TextView = itemView.findViewById(R.id.coin_card_value)
 
     }
 
-    private val layoutInflater = LayoutInflater.from(context)
+    private val inflater = LayoutInflater.from(context)
 
     // Cached copy of coins.
     private var coins = emptyList<Coin>()
@@ -26,11 +29,17 @@ class CoinListAdapter(context: Context): RecyclerView.Adapter<CoinListAdapter.Co
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinViewHolder =
-        CoinViewHolder(layoutInflater.inflate(R.layout.recyclerview_item, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return CoinViewHolder(itemView)
+    }
 
     override fun onBindViewHolder(holder: CoinViewHolder, position: Int) {
-        holder.coinItemView.text = coins[position].id
+        val currentCoin = coins[position]
+
+        holder.coinCurrency.text = currentCoin.currency
+        holder.coinId.text = currentCoin.id
+        holder.coinValue.text = currentCoin.storedValue.toString()
     }
 
     internal fun setCoins(coins: List<Coin>) {
