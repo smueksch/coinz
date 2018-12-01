ca926a2c53f1230d33b041ab1c3e748ded71b8f8
smueksch <sebastian.mueksch@gmail.com>
2018-12-01 20:23:59 +0000

    Added create account layout, basic implementation of CreateAccountActivity (no account creation implemented yet).

--- app/src/main/java/com/coinz/app/activities/CreateAccountActivity.kt
+++ app/src/main/java/com/coinz/app/activities/CreateAccountActivity.kt
@@ -1,7 +1,30 @@
 package com.coinz.app.activities
 
+import android.content.Intent
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.coinz.app.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.content_create_account.*
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    companion object {
+        const val tag = "CreateAccountActivity"
+    }
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+        auth = FirebaseAuth.getInstance()
+
+        create_acc_log_in_button.setOnClickListener {
+            startActivity(Intent(this, LogInActivity::class.java))
+            finish() // NOTE: not sure if we should be calling finish() here.
+        }
+    }
 
 }
